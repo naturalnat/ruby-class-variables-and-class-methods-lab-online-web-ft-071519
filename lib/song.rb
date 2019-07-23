5a48b119925f61f29738a46714e8c46a0222a313
Natalie Taktachev <ntaktach@gmail.com>
2019-07-23 16:36:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,28 +22,28 @@ class Song
   def self.artists
   @@artists.uniq
   end
-  
+
   def self.artist_count
       artist_count = {}
       @@artists.each do |artist|
         if artist_count[artist]
-          artist_count[artist] += 1 
+          artist_count[artist] += 1
         else
           artist_count[artist] = 1
         end
       end
       artist_count
     end
-  
+
     def self.genres
       @@genres.uniq
     end
-  
+
     def self.genre_count
       genre_count = {}
       @@genres.each do |genre|
         if genre_count[genre]
-          genre_count[genre] += 1 
+          genre_count[genre] += 1
         else
           genre_count[genre] = 1
         end
