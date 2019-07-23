dd1e6ff62c56f4e37bc4118cc681dfc139860008
Natalie Taktachev <ntaktach@gmail.com>
2019-07-23 16:46:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,7 +27,15 @@ class Song
     @@genres.uniq
   end
 
-  def self.count
-  newhash = {}
-
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
 end
